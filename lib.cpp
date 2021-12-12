8e49d47b02ecd8a111d0907e4d8de49ab3a79aec
Eddoru <91669196+Eddoru@users.noreply.github.com>
2021-12-12 14:45:13 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
 
+#include "lib.h"
+#include <cstring>
+
+bool search (char tab [10][20], char name [1][20], int &pos){
+    for(int i=0; i<10; i++){
+        if (strcmp (tab[i], name[0])==0){
+
+            pos=i;
+            return true;
+        }
+
+
+    }
+
+    return false;
+}
+
